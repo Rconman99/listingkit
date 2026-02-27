399ab574590be48bcd5347a31d576352564b186d
Rconman99 <50714103+Rconman99@users.noreply.github.com>
2026-02-27 12:03:32 -0700

    feat: HashRouter routing + app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Generate from './pages/Generate';
+import Settings from './pages/Settings';
+import History from './pages/History';
+
 export default function App() {
-  return <div>ListingKit</div>
+  return (
+    <ErrorBoundary>
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/generate" element={<Generate />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </Layout>
+        <Toaster position="bottom-right" />
+      </HashRouter>
+    </ErrorBoundary>
+  );
 }
